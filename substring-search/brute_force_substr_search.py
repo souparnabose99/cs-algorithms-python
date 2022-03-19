17fc0baefcd203bd00a2c0a49793da799ab846aa
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-19 11:50:54 +0530

    Update brute_force_substr_search.py

--- substring-search/brute_force_substr_search.py
+++ substring-search/brute_force_substr_search.py
@@ -1,3 +1,20 @@
 
-def naive_search():
-  return None
+def naive_search(pattern, text):
+    n = len(text)
+    m = len(pattern)
+    # Do single character matching from left to right, shift by 1 if there is a mismatch
+
+    # O(N)
+    for i in range(n-m+1):
+        j = 0
+        # Worst case : O(N*M)
+        while j < m:
+            if text[i+j] != pattern[j]:
+                break
+
+            j += 1
+        if j == m:
+            print("Found matching pattern at index : ", i)
+
+
+naive_search("abc", "dhe ua bibabc")
