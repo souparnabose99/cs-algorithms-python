8607ae68bb358378712f2952ecde0e87666da276
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-21 21:53:30 +0530

    Update head_tail_recursion.py

--- recursion/head_tail_recursion.py
+++ recursion/head_tail_recursion.py
@@ -1,1 +1,22 @@
-
+# Calling tail with n =  5
+# 5
+# Calling tail with n =  4
+# 4
+# Calling tail with n =  3
+# 3
+# Calling tail with n =  2
+# 2
+# Calling tail with n =  1
+# 1
+# Calling tail with n =  0
+# Calling head with n =  5
+# Calling head with n =  4
+# Calling head with n =  3
+# Calling head with n =  2
+# Calling head with n =  1
+# Calling head with n =  0
+# 1
+# 2
+# 3
+# 4
+# 5
