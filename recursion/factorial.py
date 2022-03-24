e997401d300b7c5cfa23df68fc4f5e142919ebcb
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-24 12:10:31 +0530

    Update factorial.py

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -4,6 +4,8 @@ def factorial_head(n):
         return 1
     sub_result_1 = factorial_head(n-1)
     result = n*sub_result_1
+    # We return to previous stack frame every time until stack is empty
+    # Last stack frame on the stack memory is the result of the factorial function      
     return result
 
 
