00633513054ae9dfb8f318f3984adc0e8481188b
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-26 23:41:25 +0530

    Update fibonacci_recursion.py

--- recursion/fibonacci_recursion.py
+++ recursion/fibonacci_recursion.py
@@ -13,5 +13,14 @@ def fibonacci_head(n):
     return result
 
 
+def fibonacci_tail(n, a=0, b=1):
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+
+    return fibonacci_tail(n - 1, b, a + b)
+
+
 print(fibonacci_head(3))
 
