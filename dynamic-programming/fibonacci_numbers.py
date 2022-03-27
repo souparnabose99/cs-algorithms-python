d9e98f7b62b6a3be302d80e619b455677fef451c
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-27 20:54:08 +0530

    Update fibonacci_numbers.py

--- dynamic-programming/fibonacci_numbers.py
+++ dynamic-programming/fibonacci_numbers.py
@@ -1,14 +1,14 @@
 
-
+# Exponential runtime complexity
 def fibonacci_recursion(n):
     if n == 0:
-        return 1
+        return 0
     elif n == 1:
         return 1
     return fibonacci_recursion(n-1) + fibonacci_recursion(n-2)
 
 
-# Top-down approach
+# Top-down approach, O(N) runtime complexity
 def fibonacci_memoization(n, table):
     if n not in table:
         table[n] = fibonacci_memoization(n-1, table) + fibonacci_memoization(n-2, table)
@@ -17,8 +17,20 @@ def fibonacci_memoization(n, table):
     return table[n]
 
 
-print("With DP/Memoization : ", fibonacci_memoization(5, {0: 1, 1: 1}))
-print("Without DP/Memoization : ", fibonacci_recursion(5))
+# Bottom-up approach, O(N) runtime complexity
+def fibonacci_tabulation(n, table):
+    for i in range(2, n+1):
+        table[i] = table[i-1] + table[i-2]
 
+    return table[n]
+
+
+print("With DP/Memoization : ", fibonacci_memoization(6, {0: 0, 1: 1}))
+print("With DP/Memoization : ", fibonacci_tabulation(6, {0: 0, 1: 1}))
+print("Without DP/Memoization : ", fibonacci_recursion(6))
+
+# With DP/Memoization :  8
+# With DP/Memoization :  8
+# Without DP/Memoization :  8
 
 
