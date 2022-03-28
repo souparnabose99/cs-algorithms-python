00a7b5e305e378d4cbaa570b55009a863644f5e9
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-28 13:03:05 +0530

    Update quick_sort.py

--- sorting-algorithms/quick_sort.py
+++ sorting-algorithms/quick_sort.py
@@ -1,3 +1,4 @@
+
 # O(NLogN) runtime complexity -> O(N^2) for worst case
 class QuickSort:
 
@@ -15,3 +16,27 @@ class QuickSort:
         # Do recursive call on left and right sub-array
         self.quick_sort(low, pivot_index - 1)
         self.quick_sort(pivot_index + 1, high)
+
+    def partition(self, low, high):
+        pivot = (low + high) // 2
+        self.data[pivot], self.data[high] = self.data[high], self.data[pivot]
+        # Consider all the other items and compare them with the pivot
+        for j in range(low, high):
+            if self.data[j] <= self.data[high]:
+                self.data[low], self.data[j] = self.data[j], self.data[low]
+                low += 1
+
+        # After all item consideration, swap pivot with low
+        self.data[low], self.data[high] = self.data[high], self.data[low]
+
+        return low
+
+
+if __name__ == "__main__":
+    data = [1, -4, 0, 15, 3, 97, 19, 54, 23]
+    print(data)
+    qs = QuickSort(data)
+    qs.sort()
+    print("Sorted data : ", data)
+
+
