3127d288d8d588ef3ca29231af441342190adc4b
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-03-28 13:02:50 +0530

    Update quick_sort.py

--- sorting-algorithms/quick_sort.py
+++ sorting-algorithms/quick_sort.py
@@ -1,1 +1,17 @@
+# O(NLogN) runtime complexity -> O(N^2) for worst case
+class QuickSort:
 
+    def __init__(self, data):
+        self.data = data
+
+    def sort(self):
+        self.quick_sort(0, len(self.data)-1)
+
+    # Low is index of first item, high is index of last item
+    def quick_sort(self, low, high):
+        if low >= high:
+            return
+        pivot_index = self.partition(low, high)
+        # Do recursive call on left and right sub-array
+        self.quick_sort(low, pivot_index - 1)
+        self.quick_sort(pivot_index + 1, high)
