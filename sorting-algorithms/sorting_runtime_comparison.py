07f44a397b76f540b89974e0bffeba5481c27687
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-01 16:51:19 +0530

    Update sorting_runtime_comparison.py

--- sorting-algorithms/sorting_runtime_comparison.py
+++ sorting-algorithms/sorting_runtime_comparison.py
@@ -1,1 +1,25 @@
 
+import random
+from quick_sort import QuickSort as quick
+from radix_sort import RadixSort as radix
+import time
+
+n = [n for n in range(10000)]
+random.shuffle(n)
+
+quick_sort = quick(n)
+
+t = time.time()
+quick_sort.sort()
+print('Quicksort time taken: %s' % str(time.time() - t))
+
+radix_sort = radix(n)
+
+random.shuffle(n)
+
+t = time.time()
+radix_sort.sort()
+print('Radix sort time taken: %s' % str(time.time() - t))
+
+# Quicksort time taken: 0.12093067169189453
+# Radix sort time taken: 0.04897117614746094
