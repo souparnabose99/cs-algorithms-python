8857d6beae6ab17af21d54a8f856ecac6a4aae92
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-04-30 19:25:39 +0530

    Update tower_of_hanoi.py

--- recursion/tower_of_hanoi.py
+++ recursion/tower_of_hanoi.py
@@ -1,1 +1,17 @@
 
+# O(2^N) Exponential runtime complexity
+def hanoi_impl(disk, source, middle, destination):
+
+    # Base case: Index 0 implies smallest disc and 2 is largest
+    if disk == 0:
+        print("Disk %s from %s to %s" % (disk, source, destination))
+        return
+
+    # moving n-1 disks
+    hanoi_impl(disk - 1, source, destination, middle)
+    print("Disk %s from %s to %s" % (disk, source, destination))
+    hanoi_impl(disk - 1, middle, source, destination)
+
+
+hanoi_impl(2, "A", "B", "C")
+
